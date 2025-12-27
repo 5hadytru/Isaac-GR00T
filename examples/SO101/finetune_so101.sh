85ce8509a5d6d36b5aa996989b4206ddaa285452
Truman <thicko18@yahoo.com>
2025-12-27 08:46:21 +0000

    Conditional barrier

--- examples/SO101/finetune_so101.sh
+++ examples/SO101/finetune_so101.sh
@@ -1,26 +1,11 @@
 set -x -e
 
 """
-from huggingface_hub import snapshot_download
-from huggingface_hub import login
-
-login()
-
-snapshot_download(
-    repo_id="5hadytru/so101_grasp_2",
-    repo_type="dataset",
-    revision="v3.0",
-    local_dir="/workspace/",
-    local_dir_use_symlinks=False,
-)
-
-d = "so101_grasp_2"
-snapshot_download(
-    repo_id="5hadytru/{d}",
-    repo_type="dataset",
-    local_dir=f"{d}",
-    local_dir_use_symlinks=False,
-)
+huggingface-cli download \
+  --repo-type dataset \
+  5hadytru/so101_bench_real_1_v2.1 \
+  --local-dir /workspace/Isaac-GR00T/examples/SO101/so101_bench_real_1_v2.1 \
+  --local-dir-use-symlinks False
 """
 
 export NUM_GPUS=1
@@ -29,12 +14,12 @@ export HUGGINGFACE_HUB_CACHE="/workspace/hf_cache"
 CUDA_VISIBLE_DEVICES=0 uv run python \
     gr00t/experiment/launch_finetune.py \
     --base_model_path nvidia/GR00T-N1.6-3B \
-    --dataset_path  examples/SO101/so101_grasp_2 examples/SO101/so101_IF_1_v2.1 \
+    --dataset_path  examples/SO101/so101_bench_real_1_v2.1 \
     --modality_config_path examples/SO101/so101_config.py \
     --embodiment_tag NEW_EMBODIMENT \
     --num_gpus $NUM_GPUS \
     --output_dir /workspace/so101_GR00T-N1.6-3B_v5_1 \
-    --save_steps 1000 \
+    --save_steps 250 \
     --save_total_limit 1 \
     --max_steps 60000 \
     --warmup_ratio 0.05 \
