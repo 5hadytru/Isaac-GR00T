8e3a83c2f28b3432b9565a275d69e266a0704451
Truman <thicko18@yahoo.com>
2026-07-18 19:32:11 +0000

    Updated fine-tuning command

--- examples/SO101/finetune_so101.sh
+++ examples/SO101/finetune_so101.sh
@@ -1,36 +1,23 @@
 set -x -e
 
-"""
-huggingface-cli download \
-  --repo-type dataset \
-  5hadytru/so101_bench_real_2_v2.1 \
-  --local-dir /workspace/Isaac-GR00T/examples/SO101/so101_bench_real_2_v2.1 \
-  --local-dir-use-symlinks False
-"""
-
 export NUM_GPUS=1
 source /workspace/export_vars.sh
 export HUGGINGFACE_HUB_CACHE="/workspace/hf_cache"
 CUDA_VISIBLE_DEVICES=0 uv run python \
     gr00t/experiment/launch_finetune.py \
     --base_model_path nvidia/GR00T-N1.6-3B \
-    --dataset_path  examples/SO101/so101_bench_real_2_v2.1 \
+    --dataset_path  examples/SO101/so101_bench_sim_4_WM \
     --modality_config_path examples/SO101/so101_config.py \
     --embodiment_tag NEW_EMBODIMENT \
     --num_gpus $NUM_GPUS \
-    --output_dir /workspace/so101_GR00T-N1.6-3B_WM_v6 \
-    --save_steps 1000 \
-    --save_total_limit 1 \
-    --max_steps 60000 \
+    --output_dir /workspace/so101_GR00T-N1.6-3B_WM_sim_v1 \
+    --save_steps 2500 \
+    --save_total_limit 8 \
+    --max_steps 21000 \
     --warmup_ratio 0.05 \
     --weight_decay 1e-5 \
     --learning_rate 1e-4 \
     --use_wandb \
-    --global_batch_size 128 \
+    --global_batch_size 112 \
     --color_jitter_params brightness 0.3 contrast 0.4 saturation 0.5 hue 0.08 \
     --dataloader_num_workers 4
-
-"""
-uv run python scripts/lerobot_conversion/convert_v3_to_v2.py --repo-id 5hadytru/so101_grasp_3 --root examples/SO101/so101_grasp_3
-cp modality.json examples/SO101/so101_grasp_3/meta/modality.json
-"""
